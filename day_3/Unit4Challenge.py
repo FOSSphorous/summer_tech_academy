ddc678fbb9343897f8e5a97846ac3d09e7f6fc09
FOSSphorous <fossphorous@outlook.com>
2022-08-03 13:30:45 -0400

    Finished Unit 4, proud of myself 🦾

---
+++ day_3/Unit4Challenge.py
@@ -0,0 +1,127 @@
+#region VEXcode Generated Robot Configuration
+import math
+import random
+from vexcode_vr_enhanced_robot import *
+
+drivetrain = Drivetrain()
+magnet = Electromagnet("magnet", 0)
+pen = Pen()
+brain = Brain()
+left_bumper = Bumper("leftBumper", 1)
+right_bumper = Bumper("rightBumper", 2)
+front_eye = EyeSensor("fronteye", 3)
+down_eye = EyeSensor("downeye", 4)
+right_eye = EyeSensor("righteye", 5)
+left_eye = EyeSensor("lefteye", 6)
+rear_eye = EyeSensor("reareye", 7)
+front_distance = Distance("frontdistance", 8)
+rear_distance = Distance("reardistance", 9)
+left_distance = Distance("leftdistance", 10)
+right_distance = Distance("rightdistance", 11)
+location = Location()
+pen.set_pen_width(THIN)
+distance = front_distance
+#endregion VEXcode Generated Robot Configuration
+
+
+def main():
+  # Set speeds to max to save some time in the sim
+  drivetrain.set_drive_velocity(100, PERCENT)
+  drivetrain.set_turn_velocity(100, PERCENT)
+
+  while not left_bumper.pressed():
+    drivetrain.drive(FORWARD)
+    wait(5, MSEC)
+
+  drivetrain.turn_for(LEFT, 90, DEGREES) # First wall, across start
+
+  while not left_bumper. pressed():
+    drivetrain.drive(FORWARD)
+    wait(5, MSEC)
+
+  drivetrain.turn_for(RIGHT, 90, DEGREES) # Second wall, adjacent A
+
+  while not left_bumper.pressed():
+    drivetrain. drive(FORWARD)
+    wait(5, MSEC)
+
+  drivetrain.turn_for(LEFT, 90, DEGREES) # Third wall, across A
+
+  while not left_bumper.pressed():
+    drivetrain.drive(FORWARD)
+    wait(5, MSEC)
+
+  #drivetrain.stop()
+  # Fourth wall, adjacent 2
+  drivetrain.drive_for(REVERSE, 300, MM)
+  drivetrain.turn_for(RIGHT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  drivetrain.turn_for(LEFT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  drivetrain.turn_for(RIGHT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  drivetrain.turn_for(RIGHT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  drivetrain.turn_for(LEFT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  for LOOP in range(2):
+    drivetrain.turn_for(RIGHT, 90, DEGREES)
+
+    while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  drivetrain.turn_for(LEFT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+    drivetrain.drive(FORWARD)
+    wait(5, MSEC)
+
+  drivetrain.turn_for(LEFT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+        drivetrain.drive(FORWARD)
+        wait(5, MSEC)
+
+  drivetrain.drive_for(REVERSE, 300, MM)
+  drivetrain.turn_for(LEFT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+    drivetrain.drive(FORWARD)
+    wait(5, MSEC)
+
+  drivetrain.turn_for(RIGHT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  drivetrain.turn_for(LEFT, 90, DEGREES)
+
+  while not left_bumper.pressed():
+      drivetrain.drive(FORWARD)
+      wait(5, MSEC)
+
+  stop_project()
+
+vr_thread(main)
